f39d79669b61410a043c5964c504fdc03f781358
Alexis Patiño Agudelo <alexispatinoagudelo@gmail.com>
2023-11-01 04:12:51 -0500

    id order

--- src/libs/models/order.ts
+++ src/libs/models/order.ts
@@ -1,13 +1,13 @@
 import { orders } from "../database/orders";
 
 export class Order {
-    public id:number ;
+    public id:string ;
     public product: string ;
     public quantity: number  ;
     public totalPrice: number  ;
     public customer: string  ;
 
-    constructor(id:number,product:string,quantity:number,totalPrice:number,customer:string){
+    constructor(id:string,product:string,quantity:number,totalPrice:number,customer:string){
         this.id = id
         this.product=product
         this.quantity =quantity
--- src/libs/routes/orders.ts
+++ src/libs/routes/orders.ts
@@ -38,37 +38,23 @@ router.get('/order/unit', (req,res)=>{
     }
 })
 
-router.get('/order/unit', (req,res)=>{
-    try {
-        let _manager = new Manager({username:req.query.username},req);
-        _manager.authorize()
-        let orderid: number | undefined;
-        if (typeof req.query.orderid === 'string') {
-            orderid = parseInt(req.query.orderid, 10);
-        }
-        else{
-            orderid = 0;
-        }
-        const Order = orders[orderid]        
-        if(Order === undefined ){
-            res.status(404).send("Orden no encontrada")
-        }else{
-            res.status(200).send({ok:true,message:Order})
-        }
-    } catch (error) {
-        res.status(409).send(error)
-    }
-})
-
 router.post('/order',(req,res)=>{
     try {
         let _manager = new Manager({username:req.query.username},req);
+
         _manager.authorize()
         const order = req.body.order;
-
-        const user = req.body.user;        
+        function generateUUID() {
+            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+                var r = Math.random() * 16 | 0,
+                    v = c === 'x' ? r : (r & 0x3 | 0x8);
+                return v.toString(16);
+            });
+        }
         
-        const _order = new Order(order.id,order.product,order.quantity,order.totalPrice,order.customer);
+        const uuid = generateUUID();
+        
+        const _order = new Order(uuid,order.product,order.quantity,order.totalPrice,order.customer);
         _order.createOrder(_order)
 
         res.status(200).send({ok:true,message:"Order saved."})
