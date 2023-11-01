31140f53f9b02afcb09a3aec2cf63ee8ad3d3ea3
JoseMarulandaLuguu <josemarulanda.luguu@gmail.com>
2023-11-01 01:48:23 -0500

    Login and IP verification completed

--- src/libs/models/authenticator.ts
+++ src/libs/models/authenticator.ts
@@ -1,13 +1,21 @@
 import { user } from "./types/user";
 import { IVerificator } from "./IVerificator";
-import {User} from './user';
+import { users } from "../database/users";
 
 
 export class Authenticator implements IVerificator{
-    verify(user:user) {
+    verify(user:user) {        
+        let dbUser = users[user.username];
+        
+        if (!dbUser) throw 'User not found'
+
+        if (user.password == dbUser.password) {
+            user.loginDetails.logged = true;       
+        }else{
+            user.loginDetails.logged = false;       
+        }
         
         delete user.password;
-        user.loginDetails.logged = false;        
         return user;
     }
     
