ef9a542ce744f38687721dc5f5e5014a71596a37
JoseMarulandaLuguu <josemarulanda.luguu@gmail.com>
2023-10-31 19:50:20 -0500

    Project initialization

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+//Import dependencies
+import express from 'express';
+import cors from 'cors';
+//Rutas
+//import  from './'
+//import  from './'
+
+const app = express();
+//Cors
+
+app.use(cors())
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+//app.use(express.json());
+app.use((err:Error, req:express.Request, res:express.Response, next:express.NextFunction)=>{
+    res.status(400).json({message:err.message});
+});
+
+//app.use()
+
+
+//Endpoint
+app.get('/', async (req,res) =>{
+    res.send('SERVIDOR SERVICIO DE ORDENES');
+});
+
+//routes
+const port=3003;
+//Levantamiento
+ app.listen(port || 5000, () => {
+    console.log(`Escuchando API en http://localhost:${port}`);
+ });
+
+//Begining of development elements
+process.on('unhandledRejection', function(reason, promise) {
+    console.log(promise);
+});
+//End of development elements
